58ce9516056cf3e27fdf061bc85fd1a8ac6a1e0c
Cyclops <vnikita3@gmail.com>
2023-02-07 18:20:45 +0300

    Создание хранилища городов
    Созданы функции для создания списка и отображения

---
+++ internal/storage/sities.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"github.com/CyclopsV/cities-informer-skillbox/internal/models"
+	"log"
+)
+
+type Cities map[uint16]*models.City
+
+func (cs *Cities) String() string {
+	sitiesInfoStr := "Иформация о городах:\n"
+	for _, city := range *cs {
+		cityStr := city.String()
+		sitiesInfoStr += "\t" + cityStr
+	}
+	return sitiesInfoStr
+}
+
+func (cs *Cities) Create(rawInfo [][]string) {
+	for _, raw := range rawInfo {
+		city := models.City{}
+		if err := city.Create(raw); err != nil {
+			log.Printf("Ошибка распознания информации о городе: %#v\n\terr: %v\n", raw, err)
+			continue
+		}
+		(*cs)[city.ID] = &city
+	}
+}
