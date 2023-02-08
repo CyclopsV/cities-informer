3cebbcbe7e1f8b1575c6177a88ed021abfbbb535
Cyclops <vnikita3@gmail.com>
2023-02-08 19:35:41 +0300

    Создан маршрутизатор приложения
    добавлен адрес для получения города по ID

---
+++ api/routes.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func CreateRoutes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.AllowContentType("application/json"))
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", getCityByIdHandler)
+
+	})
+
+	return r
+}
