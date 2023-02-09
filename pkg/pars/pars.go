b8cae98f6cdd8aca08fbaf9956078b9baec77041
Cyclops <vnikita3@gmail.com>
2023-02-10 01:11:09 +0300

    Добавлена функция для проверки присланых полей

--- pkg/pars/pars.go
+++ pkg/pars/pars.go
@@ -3,8 +3,10 @@ package pars
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 )
 
@@ -37,4 +39,46 @@ func ParseResponseToJSON(body io.ReadCloser) (map[string]interface{}, error) {
 		err = json.Unmarshal(content, &message)
 	}
 	return message, err
+}
+
+/*
+CheckFields
+
+	data - Присланные данные
+	fields - Поля которые должны присутсвовать в присланных данных
+		формат: map[name]type
+*/
+func CheckFields(data map[string]interface{}, fields map[string]string) error {
+	for fieldName := range fields {
+		if _, ok := data[fieldName]; !ok {
+			return fmt.Errorf("отсутсвует поле `%v`", fieldName)
+		}
+	}
+
+	for valName, val := range data {
+		if _, ok := fields[valName]; !ok {
+			continue
+		}
+
+		valFloat, ok := val.(float64)
+		statusType := true
+		switch fields[valName] {
+		case "uint16":
+			if !ok || valFloat > math.MaxUint16 || valFloat < 0 {
+				statusType = false
+			}
+		case "uint32":
+			if !ok || valFloat > math.MaxInt32 || valFloat < 0 {
+				statusType = false
+			}
+		case "string":
+			if _, ok = val.(string); !ok {
+				statusType = false
+			}
+		}
+		if !statusType {
+			return fmt.Errorf("поле `%v` имеет неверный формат", valName)
+		}
+	}
+	return nil
 }
